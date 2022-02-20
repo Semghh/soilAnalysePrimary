364191e0246cb1a6faccd2469e29fe7e5a06fe71
Sgmehh <31950913+Sgmehh@users.noreply.github.com>
2022-02-20 22:34:19 +0800

    修复Bug

--- content2/src/main/java/com/example/content2/Service/Impl/Fun1CostServiceImpl.java
+++ content2/src/main/java/com/example/content2/Service/Impl/Fun1CostServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.content2.Service.Impl;
 import com.example.content2.Mapper.Fun1CostMapper;
 import com.example.content2.POJO.Fun1Cost;
 import com.example.content2.Service.Fun1CostService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service("Fun1CostService")
 public class Fun1CostServiceImpl implements Fun1CostService {
 
     @Resource
